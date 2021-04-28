db59d6c41d01d19744501ad841f7596a9da1981c
Abhisek Roy <abhisek.kai@gmail.com>
2021-04-28 09:24:20 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ShooterGame
+
+Developed with Unreal Engine 4. It is a sandbox to learn characters, animations, blueprints, level design, shooting and a lot of other cool stuffs.
---
+++ Source/ShooterGame/Public/ShooterCharacter.h
@@ -0,0 +1,34 @@
+// 2021 Copyrighted, Abhisek Roy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class SHOOTERGAME_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+};
+
